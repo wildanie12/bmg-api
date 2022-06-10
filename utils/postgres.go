e7755b88b2743f08952f234853f9e6c117d9dd78
Badar Wildanie <wildanie12@gmail.com>
2022-06-11 01:38:45 +0700

    feat: added postgres connection

---
+++ utils/postgres.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+	_config "gin-bmg-restful/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+func NewPostgresConnection(config *_config.AppConfig) *gorm.DB {
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Jakarta", 
+		config.DB.Host,
+		config.DB.Username,
+		config.DB.Password,
+		config.DB.Name,
+	)
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
