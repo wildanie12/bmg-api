1f11e72e16e44ecf7c21e978e1339052d95a0379
Badar Wildanie <wildanie12@gmail.com>
2022-06-11 21:35:01 +0700

    fix: changed code to integer value

--- entities/web/response.go
+++ entities/web/response.go
@@ -4,7 +4,7 @@ package web
 // representing success of an endpoint hit
 type SuccessResponse struct {
 	Status string `json:"status"`
-	Code string	`json:"code"`
+	Code int	`json:"code"`
 	Data string `json:"data"`
 }
 
@@ -12,14 +12,14 @@ type SuccessResponse struct {
 // on every failed validation endpoint hit
 type ValidationErrorResponse struct {
 	Status string `json:"status"`
-	Code string `json:"code"`
+	Code int `json:"code"`
 	Error string `json:"error"`
-	Errors ValidationError `json:"errors"`
+	Errors []ValidationErrorItem `json:"errors"`
 }
 
 // ErrorResponse for giving web error response
 type ErrorResponse struct {
 	Status string `josn:"status"`
-	Code string `json:"code"`
+	Code int `json:"code"`
 	Error string `json:"error"`
 }
