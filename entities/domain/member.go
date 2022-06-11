7e2573fe3c9834bd527486263bcc0df2ffc59c09
Badar Wildanie <wildanie12@gmail.com>
2022-06-11 10:25:58 +0700

    docs: added documentaion for user entity

--- entities/domain/member.go
+++ entities/domain/member.go
@@ -1,6 +1,8 @@
 package domain
 
-type Member struct {
+// User entity reponsible for database migration
+// and domain level manipulation data modeling
+type User struct {
 	Username string `gorm:"primaryKey"`
 	Password string `gorm:"size:255"`
 	Name string `gorm:"size:255"`
