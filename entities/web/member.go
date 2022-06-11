8b04f713e4cca2d7d184d6c4a1db8caa8478a2ea
Badar Wildanie <wildanie12@gmail.com>
2022-06-11 14:52:37 +0700

    add: added user request for registration

--- entities/web/member.go
+++ entities/web/member.go
@@ -1,5 +1,13 @@
 package web
 
+// UserRequest formatting entity
+type UserRequest struct {
+	Username string `form:"username" validate:"required"`
+	Password string `form:"password" validate:"required"`
+	Name string `form:"name" validate:"required"`
+	Email string `form:"email" validate:"required"`
+}
+
 // UserResponse formatting entity
 type UserResponse struct {
 	Username string `json:"username"`
