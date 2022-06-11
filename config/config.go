7f0eae66886bb8fd72bd6364fed3788f36daddbc
Badar Wildanie <wildanie12@gmail.com>
2022-06-11 12:56:35 +0700

    add: db test configuration

--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// AppConfig main struct
 type AppConfig struct {
 	App struct {
 		BaseURL string
@@ -19,10 +20,17 @@ type AppConfig struct {
 		Password string
 		Host string
 	}
+	DBTest struct {
+		Name string
+		Username string
+		Password string
+		Host string
+	}
 }
 
 var appConfig *AppConfig
 
+// New return new or old instance from initiated app config
 func New() *AppConfig {
 	
 	if appConfig == nil {
@@ -42,13 +50,21 @@ func initConfig() *AppConfig {
 		config.DB.Username = "root"
 		config.DB.Password = ""
 		config.DB.Host = "localhost"
-		} else {
-			config.App.BaseURL = os.Getenv("APP_BASE_URL")
-			config.App.Port = os.Getenv("APP_PORT")
-			config.DB.Name = os.Getenv("DB_NAME")
-			config.DB.Username = os.Getenv("DB_USER")
-			config.DB.Password = os.Getenv("DB_PASS")
-			config.DB.Host = os.Getenv("DB_HOST")
+		config.DBTest.Name = os.Getenv("DB_NAME")
+		config.DBTest.Username = os.Getenv("DB_USER")
+		config.DBTest.Password = os.Getenv("DB_PASS")
+		config.DBTest.Host = os.Getenv("DB_HOST")
+	} else {
+		config.App.BaseURL = os.Getenv("APP_BASE_URL")
+		config.App.Port = os.Getenv("APP_PORT")
+		config.DB.Name = os.Getenv("DB_NAME")
+		config.DB.Username = os.Getenv("DB_USER")
+		config.DB.Password = os.Getenv("DB_PASS")
+		config.DB.Host = os.Getenv("DB_HOST")
+		config.DBTest.Name = os.Getenv("DB_TEST_NAME")
+		config.DBTest.Username = os.Getenv("DB_TEST_USER")
+		config.DBTest.Password = os.Getenv("DB_TEST_PASS")
+		config.DBTest.Host = os.Getenv("DB_TEST_HOST")
 	}
 	config.App.URL = strings.TrimRight(config.App.BaseURL, "/") + map[bool]string{ true: "", false: config.App.Port }[config.App.Port == "80"]
 
